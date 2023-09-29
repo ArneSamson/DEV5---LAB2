3ef8a42fa9e3b78f50365a1388bb24d6a56ee7f6
ArneSamson <arnesamson007@gmail.com>
2023-09-29 14:33:54 +0200

    feat: add App class with functions to press buttons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
 import Island from "./classes/Island";
 import World from "./classes/World";
 
+export default class App {
+    constructor() {
+        this.world = new World();
+        this.hookEvents();
+    }
+    
+    hookEvents() {
+        document.querySelector("#btnAddIsland").addEventListener("click", () => {
+        this.world.addIsland(new Island());
+        });
+    
+        document.querySelector("#btnSave").addEventListener("click", () => {
+        this.world.save();
+        });
+    
+        document.querySelector("#btnLoad").addEventListener("click", () => {
+        this.world.load();
+        });
+    }
+}
