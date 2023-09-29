6194c766611f857d3e8c439b18cc6ac9b7ff408d
ArneSamson <arnesamson007@gmail.com>
2023-09-29 14:31:10 +0200

    feat: apply coordinates to island

--- src/classes/World.js
+++ src/classes/World.js
@@ -1,4 +1,5 @@
-class World {
+import Island from "./Island.js";
+export default class World {
     constructor() {
       this.islands = []; // a good place to keep track of your islands
       this.hookEvents(); // let's kick things of by hooking up events
@@ -9,30 +10,42 @@ class World {
     }
   
     save() {
-      // save array islands to localstorage as string
-      // loop over all this.islands and save the names
+      // save islands to localstorage
+      localStorage.setItem("islands", JSON.stringify(this.islands));
     }
   
     load() {
       // load islands from localstorage into array
-      // loop over the array and addIslands()
+      const savedIslands = JSON.parse(localStorage.getItem("islands")) || [];
+        savedIslands.forEach(islandData => {
+            const island = new Island();
+            island.name = islandData.name;
+            island.color = islandData.color;
+            island.position = islandData.position;
+            this.addIsland(island);
+        });
     }
   
     getCoordinates() {
+      const offset = 0.7;
       // return coordinates within the screen at random, feel free to change it up!
-      let randomSign = Math.random() < 0.5 ? -1 : 1;
-      return {
-        x: ((Math.random() * window.innerWidth) / 2) * randomSign,
-        y: ((Math.random() * window.innerHeight) / 2) * randomSign
-      };
+      const x = Math.floor(Math.random() * window.innerWidth * offset);
+      const y = Math.floor(Math.random() * window.innerHeight * offset);
+      return { x, y };
     }
   
-    addIsland(island) {
-      // add the islands to the DOM
+    addIsland(island) {     
+      this.islands.push(island);
+      this.moveIsland(island);
     }
   
     moveIsland(island) {
-      // this might be a good point to animate the islands with JS Animations API
+      // get random coordinates
+      const coordinates = this.getCoordinates();
+      // set the coordinates to the island
+      island.element.style.left = `${coordinates.x}px`;
+      island.element.style.top = `${coordinates.y}px`;
+
     }
   }
   
