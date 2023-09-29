c885b1e3317c5fb64c3a284e59c8a21e2911a9be
ArneSamson <arnesamson007@gmail.com>
2023-09-29 14:30:24 +0200

    feat: add creation of island

--- src/classes/Island.js
+++ src/classes/Island.js
@@ -1,13 +1,36 @@
 export default class Island {
-    constructor(name) {}
+    constructor() {
+      this.name = this.getRandomName();
+      this.color = this.getRandomColor();
+      this.element = this.createElement();
+      this.render();
+    }
   
     getRandomColor() {
       return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     }
+
+    createElement() {
+        const island = document.createElement("div");
+        island.classList.add("island");
+        island.style.backgroundColor = this.color;
+        island.style.transform = `translate(-50%, -50%)`;
+        island.innerHTML = `<h2>${this.name}</h2>`;
+        return island;
+    }
+
+    render() {
+        document.body.appendChild(this.element);
+    }
+
   
     remove() {
       // JS animations api, fade out
       // remove the element when the animation ended
+      this.element.style.opacity = 0;
+      setTimeout(() => {
+        this.element.remove();
+      }, timeout);
     }
   
     getRandomName() {
